3fb2f3e4729d98a09f0ad2521f36410435217709
Kira <kira.eva@protonmail.com>
2022-11-21 13:56:59 +0000

    Basic site navbar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,50 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../../assets/images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={Logo} alt="logo" />
+    </Link>
+
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#f7a194"></FontAwesomeIcon>
+        {/* <span className='navlink-text'>Home</span> */}
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        to="/about"
+        className="about-link"
+      >
+        <FontAwesomeIcon icon={faUser} color="#f7a194"></FontAwesomeIcon>
+        {/* <span className='navlink-text'>About</span> */}
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        to="/contact"
+        className="contact-link"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#f7a194"></FontAwesomeIcon>
+        {/* <span className='navlink-text'>Contact</span> */}
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/kira0x1">
+          <FontAwesomeIcon icon={faGithub} color="#f7a194"></FontAwesomeIcon>
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
