63ecfeecf5e553ff45cf8f09ad3237d9dbc875fb
Kira <kira.eva@protonmail.com>
2022-11-27 23:17:52 +0000

    use reactGA in pagetracking

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,13 +1,17 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import ReactGA from 'react-ga4'
 
 const usePageTracking = () => {
   const location = useLocation()
 
   useEffect(() => {
+    const page_path = location.pathname + location.search
     window.gtag('event', 'page_view', {
-      page_path: location.pathname + location.search,
+      page_path,
     })
+
+    ReactGA.send({ hitType: 'pageview', page: page_path })
   }, [location])
 }
 
