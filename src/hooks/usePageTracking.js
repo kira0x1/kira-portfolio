adbaabad0e85270ad2d9e26132273866552a6a68
Kira <kira.eva@protonmail.com>
2022-11-28 00:15:40 +0000

    remove redundant reactga event

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import ReactGA from 'react-ga4'
 
 const usePageTracking = () => {
   const location = useLocation()
@@ -10,8 +9,6 @@ const usePageTracking = () => {
     window.gtag('event', 'page_view', {
       page_path,
     })
-
-    ReactGA.send({ hitType: 'pageview', page: page_path })
   }, [location])
 }
 
