4c522fc18848dfb18b976558523ebb1d29c833d0
Kira <kira.eva@protonmail.com>
2022-11-29 17:00:25 +0000

    Increment version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-console.log('version: 1.2.0')
+console.log('version: 1.3.0')
 
 root.render(
   <React.StrictMode>
